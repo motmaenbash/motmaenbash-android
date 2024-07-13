3036efa250adb33f2934768af9258a3c4b2f8352
Milad <milad.nu@gmail.com>
2024-07-14 02:41:46 +0330

    Complete About Activity

--- app/src/main/java/nu/milad/motmaenbash/ui/AboutActivity.kt
+++ app/src/main/java/nu/milad/motmaenbash/ui/AboutActivity.kt
@@ -3,9 +3,8 @@ package nu.milad.motmaenbash.ui
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 import nu.milad.motmaenbash.BuildConfig
-
+import nu.milad.motmaenbash.R
 import nu.milad.motmaenbash.databinding.ActivityAboutBinding
 
 class AboutActivity : BaseActivity() {
@@ -18,15 +17,15 @@ class AboutActivity : BaseActivity() {
         binding = ActivityAboutBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.appVersion.text = "نسخه: ${BuildConfig.VERSION_NAME}"
+        binding.appVersion.text = getString(R.string.version, BuildConfig.VERSION_NAME)
 
 
         // Set up donate button
-        binding.donateButton.setOnClickListener()
-        {
-            val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://milad.nu/onate"))
+        binding.donateButton.setOnClickListener() {
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://milad.nu/page/donate"))
             startActivity(intent)
         }
+
 
     }
 }
