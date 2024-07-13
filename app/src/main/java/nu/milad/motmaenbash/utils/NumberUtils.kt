5b6a8f82d33dafe3d3ca668076606905c6d804a5
Milad <milad.nu@gmail.com>
2024-07-14 02:21:51 +0330

    Add methods to NumberUtils

--- app/src/main/java/nu/milad/motmaenbash/utils/NumberUtils.kt
+++ app/src/main/java/nu/milad/motmaenbash/utils/NumberUtils.kt
@@ -1,9 +1,27 @@
 package nu.milad.motmaenbash.utils
 
+import android.content.Context
+import nu.milad.motmaenbash.R
+import java.util.Locale
+
 object NumberUtils {
 
 
     fun formatNumber(number: Int): String {
-        return String.format("%,d", number)
+        return String.format(Locale.getDefault(), "%,d", number)
     }
+
+
+    fun toPersianNumbers(number: String): String {
+
+        val persianDigits = charArrayOf('۰', '۱', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '۹')
+        return number.map {
+            when (it) {
+                in '0'..'9' -> persianDigits[it - '0']
+                else -> it
+            }
+        }.joinToString("")
+    }
+
+
 }
